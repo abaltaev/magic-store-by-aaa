20313ce0edc73aa82f219b2d1c01a11f909f06de
Aleksey P <pravdin@my.com>
2021-03-17 21:06:20 +0300

    fake commit

---
+++ middleware/authHandler.js
@@ -0,0 +1,27 @@
+function cookieCleaner(req,res,next){
+  if(req.cookie.user_sid && !req.session.user){
+    res.clearCookie('user_sid')
+  }
+  next()
+}
+
+function sessionChecker(req ,res, next){
+  if(req.session.user){
+    alert('Вы уже залогинины')
+    res.redirect('/') //???? ручка под вопросом
+  } else{
+    next()
+  }
+}
+
+
+const checkUser = (req, res, next) => {
+  if(req.session.user){
+    res.locals.check = true
+    next()
+  } else {
+    res.locals.check = false
+    next()
+  }
+}
+module.exports = {sessionChecker, cookieCleaner, checkUser}
