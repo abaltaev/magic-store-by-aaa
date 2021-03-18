970a4378434b3739529e1a599802ddeb6630f7d8
Aleksey P <pravdin@my.com>
2021-03-18 12:26:23 +0300

    profile-forsale-purchased

--- routes/auth.js
+++ routes/auth.js
@@ -1,49 +1,45 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
 const { sessionChecker } = require('../middleware/authHandler')
-const { cookieCleaner } = require('../middleware/authHandler')
 const User = require('../models/user')
 
-// const { route } = require('./entries')
 
-router.get('/', (req, res)=>{
-  res.render('index')
-})
 
 router
   .route('/login')
   .get(sessionChecker,(req,res)=>{
-    res.render('entries/auth')
+    res.render('/login')
   })
   .post(async (req,res)=>{
     const {name, password} = req.body
     const user = await User.findOne({name})
     if(user && (await bcrypt.compare(password, user.password))){
       req.session.user = user
-      res.redirect('/entries')
+      res.redirect('/')
     }else{
-      // alert('Не верный login или password')
-      res.redirect('/auth')
+      alert('Не верный login и/или password')
+      res.redirect('/login')
     }
   })
 
 router
-  .route('/signUp')
+  .route('/signup')
   .get(sessionChecker,(req ,res)=>{
-    res.render('entries/signUp')
+    res.render('/signup')
   })
   .post(async (req,res, next)=>{
     try{
-      const {name, email, password} = req.body
+      const {name, email, password, city} = req.body
       const salt = 10
      const user = new User({
         name,
         email,
-        password: await bcrypt.hash(password,salt)
+        password: await bcrypt.hash(password,salt),
+        city
       }) 
       await user.save()
       req.session.user = user
-      res.redirect('/entries')
+      res.redirect('/')
     } catch (error){
       next(error)
     }
@@ -54,12 +50,12 @@ if(req.session.user){
   try{
     await req.session.destroy()
     res.clearCookie('user_sid')
-    res.redirect('/auth')
+    res.redirect('/')
   }catch (error){
    next(error)
   }
 }else{
-  res.redirect('/auth')
+  res.redirect('/')
 }
 })
 
