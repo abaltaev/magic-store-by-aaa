a89ae84f229bf13e046822b58d027f1997ef1407
Alexander <shyguywannafly@gmail.com>
2021-03-18 22:28:23 +0300

    slider

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 const router = require('express').Router()
+const Card = require('../models/card');
 
-router.get('/',(req,res)=>res.render('index'))
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+router.get('/getarray', async (req, res) => {
+  const cardsArr = await Card.find({ status: true });
+  res.json(cardsArr);
+})
 
 module.exports = router
