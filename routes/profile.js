746f85c6089bb3c5eab95286e44ab0f9032e8fe0
Alexander <shyguywannafly@gmail.com>
2021-03-19 14:12:51 +0300

    basket

--- routes/profile.js
+++ routes/profile.js
@@ -11,18 +11,18 @@ router.get('/forsale',(req,res)=>res.render('forsale'))
 
 router.post('/forsale', async (req, res) => {
   const arr = {
-    1: 'https://media.magic.wizards.com/image_legacy_migration/images/magic/tcg/products/dgm/tiwoirwiixix/ltap0gxvul_RU.jpg',
-    2: 'https://media.magic.wizards.com/image_legacy_migration/images/magic/tcg/products/dgm/tiwoirwiixix/sm6uf6dhow_RU.jpg',
-    3:'https://media.magic.wizards.com/image_legacy_migration/images/magic/tcg/products/dgm/tiwoirwiixix/7uyt66c4fo_RU.jpg',
-    4:'https://media.magic.wizards.com/image_legacy_migration/images/magic/tcg/products/dgm/tiwoirwiixix/fp8onqgwo3_RU.jpg',
-    5:'https://media.magic.wizards.com/image_legacy_migration/images/magic/tcg/products/dgm/tiwoirwiixix/c2o26fe2w5_RU.jpg',
-    6:'https://media.magic.wizards.com/image_legacy_migration/images/magic/tcg/products/dgm/tiwoirwiixix/cgcqubotzx_RU.jpg',
-    7:'https://media.magic.wizards.com/image_legacy_migration/images/magic/tcg/products/dgm/tiwoirwiixix/3bo69fwpy6_RU.jpg',
-    8:'https://media.magic.wizards.com/image_legacy_migration/images/magic/tcg/products/dgm/tiwoirwiixix/6uk431besl_RU.jpg',
-    9:'https://media.magic.wizards.com/image_legacy_migration/images/magic/tcg/products/dgm/tiwoirwiixix/lr7wuf741i_RU.jpg',
-    10:'https://media.magic.wizards.com/image_legacy_migration/images/magic/tcg/products/dgm/tiwoirwiixix/wg130bzl8b_RU.jpg',
-    11:'https://media.magic.wizards.com/image_legacy_migration/images/magic/tcg/products/dgm/tiwoirwiixix/hnnrutsyhl_RU.jpg',
-    12:'https://media.magic.wizards.com/image_legacy_migration/images/magic/tcg/products/dgm/tiwoirwiixix/rqj2xg44it_RU.jpg',
+    1: 'https://cdn.hearthstonetopdecks.com/wp-content/uploads/2014/03/acolyte-of-pain.png',
+    2: 'https://cdn.hearthstonetopdecks.com/wp-content/uploads/2014/11/antique-healbot1.png',
+    3: 'https://cdn.hearthstonetopdecks.com/wp-content/uploads/2016/11/abyssal-enforcer-1.png',
+    4: 'https://cdn.hearthstonetopdecks.com/wp-content/uploads/2015/08/alexstraszas-champion-hd.png',
+    5: 'https://cdn.hearthstonetopdecks.com/wp-content/uploads/2014/03/azure-drake.png',
+    6: 'https://cdn.hearthstonetopdecks.com/wp-content/uploads/2015/11/anyfin-can-happen-hd.png',
+    7: 'https://cdn.hearthstonetopdecks.com/wp-content/uploads/2014/03/doomhammer.png',
+    8: 'https://cdn.hearthstonetopdecks.com/wp-content/uploads/2014/03/ancient-of-war1.png',
+    9: 'https://cdn.hearthstonetopdecks.com/wp-content/uploads/2016/08/arcane-giant.png',
+    10:'https://cdn.hearthstonetopdecks.com/wp-content/uploads/2018/02/patches-the-pirate-nerf.png',
+    11:'https://cdn.hearthstonetopdecks.com/wp-content/uploads/2014/03/alexstrasza.png',
+    12:'https://cdn.hearthstonetopdecks.com/wp-content/uploads/2014/03/deathwing.png',
     }
 
   const card = new Card ({
